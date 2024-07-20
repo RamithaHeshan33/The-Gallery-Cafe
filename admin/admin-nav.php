6db65ddb46046f65f01642ebee26555b8e920ba8
Ramitha Heshan <st20286184@outlook.cardiffmet.ac.uk>
2024-07-20 22:26:09 +0530

    admin section

---
+++ admin/admin-nav.php
@@ -0,0 +1,140 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gallery Cafe</title>
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
+
+    <!-- Tailwind CSS (required for Flowbite) -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css" rel="stylesheet">
+    <!-- Flowbite CSS -->
+    <link href="https://unpkg.com/flowbite@1.6.5/dist/flowbite.min.css" rel="stylesheet">
+
+    <style>
+        .active-link {
+            border-bottom: 2px solid yellow;
+        }
+
+        .profile {
+            width: 30px;
+            height: 30px;
+            border-radius: 50%;
+            cursor: pointer;
+            margin-right: 10px;
+        }
+
+        .dropdown {
+            position: relative;
+            display: inline-block;
+        }
+
+        .dropdown-content {
+            display: none;
+            position: absolute;
+            right: 0;
+            background-color: #333;
+            min-width: 160px;
+            box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+            z-index: 1;
+            border-radius: 5px;
+            overflow: hidden;
+        }
+
+        .dropdown-content a {
+            color: white;
+            padding: 12px 16px;
+            text-decoration: none;
+            display: block;
+            transition: background-color 0.3s;
+        }
+
+        .dropdown-content a:hover {
+            background-color: #575757;
+        }
+
+        .dropdown:hover .dropdown-content {
+            display: block;
+        }
+
+        
+
+        @media (max-width: 390px) {
+            .dropdown-content {
+                right: auto;
+                left: 0;
+                min-width: 100px;
+            }
+
+            .profile {
+                width: 25px;
+                height: 25px;
+            }
+        }
+
+        
+
+    </style>
+</head>
+<body id="home">
+    <!-- Navbar -->
+    <nav class="bg-black border-gray-200 fixed w-full top-0 left-0 z-50">
+        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+            <a href="admin.php" class="flex items-center space-x-3 rtl:space-x-reverse">
+                <span class="self-center text-2xl font-semibold whitespace-nowrap text-white">Gallery Cafe</span>
+            </a>
+            <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-white rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+                <span class="sr-only">Open main menu</span>
+                <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+                </svg>
+            </button>
+            <div class="hidden w-full md:block md:w-auto bg-black" id="navbar-default">
+                <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-black md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-black dark:bg-gray-800 md:dark:bg-black dark:border-gray-700">
+                    <li><a href="admin.php" class="block py-2 px-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-yellow-300 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Home</a></li>
+                    <li><a href="menu.php" class="block py-2 px-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-yellow-300 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Menu</a></li>
+                    <li><a href="promo.php" class="block py-2 px-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-yellow-300 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Promotions & Offers</a></li>
+                    <li><a href="about1.php" class="block py-2 px-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-yellow-300 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About Us</a></li>
+                    <li><a href="services1.php" class="block py-2 px-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-yellow-300 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a></li>
+                    <li><a href="orders/order.php" class="block py-2 px-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-yellow-300 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Online Orders</a></li>
+                    <li><a href="reservation.php" class="block py-2 px-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-yellow-300 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Reserve</a></li>
+                </ul>
+            </div>
+            <div class="dropdown">
+                <img src="../img/profile.png" alt="" class="profile">
+                <div class="dropdown-content">
+                    <a href="profile.php">Profile</a>
+                    <a href="park-reservation.php">Parking Reservations</a>
+                    <a href="Table-reservation.php">Table Reservations</a>
+                    <a href="../index.php">Logout</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+</body>
+
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        const navLinks = document.querySelectorAll('nav ul li a');
+        const currentPath = window.location.pathname.split('/').pop();
+
+        navLinks.forEach(link => {
+            if (link.getAttribute('href') === currentPath) {
+                link.classList.add('active-link');
+            }
+            link.addEventListener('click', function() {
+                navLinks.forEach(nav => nav.classList.remove('active-link'));
+                link.classList.add('active-link');
+            });
+        });
+    });
+</script>
+
+<script src="https://unpkg.com/flowbite@1.6.5/dist/flowbite.js"></script>
+
+</html>
--- orders/add_to_cart.php
+++ orders/add_to_cart.php
@@ -2,45 +2,61 @@
 session_start();
 require '../connection.php';
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $food_id = $_POST['food_id'];
-    $food_name = $_POST['food_name'];
-    $food_price = $_POST['food_price'];
-    $username = $_SESSION['username']; // Assuming username is stored in session
+try {
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $food_id = $_POST['food_id'];
+        $food_name = $_POST['food_name'];
+        $food_price = $_POST['food_price'];
+        $username = $_SESSION['username']; // Assuming username is stored in session
 
-    // Check if the item is already in the cart
-    $check_sql = "SELECT * FROM cart WHERE username = ? AND name = ?";
-    $check_stmt = $conn->prepare($check_sql);
-    $check_stmt->bind_param('ss', $username, $food_name);
-    $check_stmt->execute();
-    $check_result = $check_stmt->get_result();
+        // Check if the item is already in the cart
+        $check_sql = "SELECT * FROM cart WHERE username = ? AND name = ?";
+        $check_stmt = $conn->prepare($check_sql);
+        $check_stmt->bind_param('ss', $username, $food_name);
+        $check_stmt->execute();
+        $check_result = $check_stmt->get_result();
 
-    if ($check_result->num_rows > 0) {
-        // If item is already in the cart, update the quantity
-        $update_sql = "UPDATE cart SET quantity = quantity + 1 WHERE username = ? AND name = ?";
-        $update_stmt = $conn->prepare($update_sql);
-        $update_stmt->bind_param('ss', $username, $food_name);
-        if ($update_stmt->execute()) {
-            header("Location: order.php?message=quantity");
+        if ($check_result->num_rows > 0) {
+            // If item is already in the cart, update the quantity
+            $update_sql = "UPDATE cart SET quantity = quantity + 1 WHERE username = ? AND name = ?";
+            $update_stmt = $conn->prepare($update_sql);
+            $update_stmt->bind_param('ss', $username, $food_name);
+            $update_stmt->execute();
+            $update_stmt->close();
+
+            $update_sql1 = "UPDATE orders SET quantity = quantity + 1 WHERE username = ? AND name = ?";
+            $update_stmt1 = $conn->prepare($update_sql1);
+            $update_stmt1->bind_param('ss', $username, $food_name);
+            if ($update_stmt1->execute()) {
+                header("Location: order.php?message=quantity");
+            } else {
+                header("Location: order.php?message=err");
+            }
+            $update_stmt1->close();
         } else {
-            header("Location: order.php?message=err");
+            // If item is not in the cart, insert new row
+            $insert_sql = "INSERT INTO cart (username, name, price, quantity) VALUES (?, ?, ?, 1)";
+            $insert_stmt = $conn->prepare($insert_sql);
+            $insert_stmt->bind_param('ssd', $username, $food_name, $food_price);
+
+            $insert_sql1 = "INSERT INTO orders (username, name, price, quantity) VALUES (?, ?, ?, 1)";
+            $insert_stmt1 = $conn->prepare($insert_sql1);
+            $insert_stmt1->bind_param('ssd', $username, $food_name, $food_price);
+            if ($insert_stmt->execute() && $insert_stmt1->execute()) {
+                header("Location: order.php?message=submitted");
+            } else {
+                header("Location: order.php?message=err");
+            }
+            $insert_stmt->close();
+            $insert_stmt1->close();
         }
-        $update_stmt->close();
-    } else {
-        // If item is not in the cart, insert new row
-        $insert_sql = "INSERT INTO cart (username, name, price, quantity) VALUES (?, ?, ?, 1)";
-        $insert_stmt = $conn->prepare($insert_sql);
-        $insert_stmt->bind_param('ssd', $username, $food_name, $food_price);
-        if ($insert_stmt->execute()) {
-            header("Location: order.php?message=submitted");
-        } else {
-            header("Location: order.php?message=err");
-        }
-        $insert_stmt->close();
+
+        $check_stmt->close();
+        $conn->close();
+        exit();
     }
-
-    $check_stmt->close();
-    $conn->close();
+} catch (Exception $e) {
+    header("Location: order.php?message=exception&error=" . $e->getMessage());
     exit();
 }
 ?>
